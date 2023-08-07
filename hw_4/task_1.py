f9ade04e637df9a238d83a5c16bcac30f52b24f0
julek <julek@mail.ru>
2023-08-07 17:48:24 +0300

    add_tasks_hw_4

---
+++ hw_4/task_1.py
@@ -0,0 +1,28 @@
+'''
+Задача 1:
+Напишите функцию для транспонирования матрицы
+'''
+
+matrix_init = [[5, 5, 1, 3],
+               [6, 7, 9, 4],
+               [8, 2, 8, 0]]
+print("Дана матрица: \n" + '\n'.join(list(map(str, matrix_init))))
+
+# Короткое решение с использованием zip() (zip() возвращает элементы в картеже -> в списке !)
+print("Транспонированная матрица (решение в одну строку с красивым выводом):\
+ \n"+'\n'.join(list(map(str, [list(line) for line in zip(*matrix_init)]))))
+
+#Функция для транспонирования матрицы
+def transpose(matrix):
+    res = []
+    row = len(matrix)
+    colum = len(matrix[0])
+    for j in range(colum):
+        tmp = []
+        for i in range(row):
+            tmp = tmp + [matrix[i][j]]
+        res = res + [tmp]
+    return res
+
+# развернуто
+print("Транспонированная матрица: \n"+'\n'.join(list(map(str, transpose(matrix_init)))))
